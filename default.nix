85a047f519436625ed4a71a310d0d73dfaadc187
Valentin Pikas <pikasvaliantsin@github.com>
2026-07-23 11:55:46 +0300

    Fix duplicate stdenv argument

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { stdenv, dpkg, autoPatchelfHook, fetchurl
 , nspr, nss, gtk3, glib, libX11, libxkbcommon, xorg
-, zlib, stdenv, alsa-lib, libpulseaudio
+, zlib, alsa-lib, libpulseaudio
 , makeWrapper
 }:
 
@@ -19,29 +19,20 @@ stdenv.mkDerivation rec {
     nspr nss gtk3 glib libX11 libxkbcommon
     xorg.libXScrnSaver xorg.libXcomposite xorg.libXdamage xorg.libXext
     xorg.libXfixes xorg.libXi xorg.libXrandr xorg.libxcb
-    zlib stdenv.cc.cc.lib alsa-lib libpulseaudio
+    zlib alsa-lib libpulseaudio
   ];
 
   unpackPhase = ''
-    # Распаковываем .deb архив
     dpkg-deb -x $src .
   '';
 
   installPhase = ''
-    # Создаём основные директории
     mkdir -p $out/bin $out/share/applications $out/share/icons/hicolor/256x256/apps
-
-    # Копируем все файлы приложения
     cp -r opt/Cliq/* $out/bin/
-
-    # Делаем бинарник исполняемым
     chmod +x $out/bin/cliq
-
-    # Копируем иконку в стандартную папку
     cp $out/bin/resources/app/app/images/appicon.png \
        $out/share/icons/hicolor/256x256/apps/zoho-cliq.png
 
-    # Создаём .desktop файл
     cat > $out/share/applications/zoho-cliq.desktop <<EOF
     [Desktop Entry]
     Name=Zoho Cliq
@@ -52,15 +43,11 @@ stdenv.mkDerivation rec {
     Categories=Network;InstantMessaging;
     EOF
 
-    # Создаём обёртку для правильного LD_LIBRARY_PATH (на случай, если autoPatchelfHook не справится)
     makeWrapper $out/bin/cliq $out/bin/.cliq-wrapper \
       --prefix LD_LIBRARY_PATH : ${stdenv.lib.makeLibraryPath buildInputs}
   '';
 
-  # autoPatchelfHook автоматически исправляет rpath бинарников
-  # а makeWrapper создаёт обёртку с LD_LIBRARY_PATH (дублируем для надёжности)
   postFixup = ''
-    # Перемещаем обёртку на место оригинального бинарника
     mv $out/bin/.cliq-wrapper $out/bin/cliq
   '';
 
@@ -69,6 +56,5 @@ stdenv.mkDerivation rec {
     homepage = "https://www.zoho.com/cliq/";
     license = stdenv.lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    maintainers = [ ];
   };
 }
