d9c369b1a4ead33bdfd334c69b00ef537ca6b123
Valentin Pikas <pikasvaliantsin@github.com>
2026-07-23 11:58:10 +0300

    Allow unfree packages in flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true; # Разрешаем установку unfree пакетов
+    };
   in {
     packages.x86_64-linux.zoho-cliq = pkgs.callPackage ./default.nix { };
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.zoho-cliq;
