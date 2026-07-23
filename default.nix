6faed54d6d3cf97f939f18f86ae934e03baa12b5
Valentin Pikas <pikasvaliantsin@github.com>
2026-07-23 22:12:02 +0300

    Fix video sharing

--- default.nix
+++ default.nix
@@ -1,72 +1,101 @@
-{ stdenv, dpkg, autoPatchelfHook, fetchurl
-, nspr, nss, gtk3, glib, libX11, libxkbcommon
-, libxscrnsaver, libxcomposite, libxdamage, libxext
-, libxfixes, libxi, libxrandr, libxcb
-, zlib, alsa-lib, libpulseaudio
-, makeWrapper
-, lib
-, bash
-, mesa
-, libgbm
-}:
+{ pkgs, lib }:
 
-stdenv.mkDerivation rec {
-  pname = "zoho-cliq";
+let
   version = "1.8.3";
-
-  src = fetchurl {
-    url = "https://downloads.zohocdn.com/chat-desktop/linux/cliq_${version}_amd64.deb";
-    hash = "sha256-9XjnIJdgdPXysXXbEdwr2cBG7OS1z6E8s+oL3hhfRGU=";
+  
+  cliq-src = pkgs.stdenv.mkDerivation {
+    name = "zoho-cliq-src-${version}";
+    src = pkgs.fetchurl {
+      url = "https://downloads.zohocdn.com/chat-desktop/linux/cliq_${version}_amd64.deb";
+      hash = "sha256-9XjnIJdgdPXysXXbEdwr2cBG7OS1z6E8s+oL3hhfRGU=";
+    };
+    nativeBuildInputs = [ pkgs.dpkg ];
+    unpackPhase = "dpkg-deb -x $src .";
+    installPhase = ''
+      mkdir -p $out
+      cp -r opt $out/
+      cp -r usr $out/
+    '';
   };
 
-  nativeBuildInputs = [ dpkg autoPatchelfHook makeWrapper ];
+  cliq-fhs = pkgs.buildFHSEnv {
+    name = "zoho-cliq-fhs";
+    
+    targetPkgs = pkgs: with pkgs; [
+      cliq-src
+      
+      # КРИТИЧНО: EGL и Wayland для PipeWire-захвата
+      libglvnd
+      wayland
+      
+      # Графика и X11
+      mesa libgbm libdrm
+      gtk3 glib pango cairo gdk-pixbuf atk at-spi2-atk
+      libX11 libxkbcommon libxcomposite libxdamage
+      libxext libxfixes libxrandr libxcb
+      
+      # Криптография
+      nspr nss
+      
+      # Звук
+      alsa-lib libpulseaudio
+      
+      # Системные
+      dbus cups expat udev
+      stdenv.cc.cc.lib
+      
+      # PipeWire и порталы
+      pipewire wireplumber
+      xdg-desktop-portal xdg-desktop-portal-gtk
+      
+      # Аппаратное декодирование
+      libva libva-utils
+    ];
 
-  buildInputs = [
-    nspr nss gtk3 glib libX11 libxkbcommon
-    libxscrnsaver libxcomposite libxdamage libxext
-    libxfixes libxi libxrandr libxcb
-    zlib stdenv.cc.cc.lib alsa-lib libpulseaudio
-    mesa
-    libgbm
-  ];
+    # Финальный оптимизированный запуск для Wayland + NixOS FHS
+    runScript = ''
+      #!/bin/sh
+      export NIXOS_OZONE_WL=1
+      
+      exec ${cliq-src}/opt/Cliq/cliq \
+        --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,CanvasOopRasterization,VaapiVideoDecoder,WaylandWindowDecorations \
+        --enable-gpu-rasterization \
+        --enable-zero-copy \
+        --disable-features=CalculateNativeWinOcclusion \
+        --disable-gpu-sandbox \
+        --ozone-platform=wayland \
+        "$@"
+    '';
+  };
 
-  unpackPhase = ''
-    dpkg-deb -x $src .
-  '';
-
+in
+pkgs.stdenv.mkDerivation {
+  name = "zoho-cliq-${version}";
+  dontUnpack = true;
+  
   installPhase = ''
-    mkdir -p $out/bin $out/share/applications $out/share/icons/hicolor/256x256/apps
-
-    cp -r opt/Cliq/* $out/bin/
-    chmod +x $out/bin/cliq
-
-    cp -r usr/share/icons/* $out/share/icons/
-
-    # Обёртка для запуска
-    cat > $out/bin/zoho-cliq <<EOF
-#!/run/current-system/sw/bin/bash
-export LD_LIBRARY_PATH=${lib.makeLibraryPath buildInputs}:$out/bin
-exec $out/bin/cliq "\$@"
-EOF
-    chmod +x $out/bin/zoho-cliq
-
-    # .desktop файл
+    mkdir -p $out/bin $out/share/applications $out/share/icons
+    
+    cp -r ${cliq-src}/usr/share/icons/* $out/share/icons/ || true
+    
     cat > $out/share/applications/zoho-cliq.desktop <<EOF
 [Desktop Entry]
 Name=Zoho Cliq
 Comment=Zoho Cliq Desktop Client
-Exec=$out/bin/zoho-cliq
+Exec=${cliq-fhs}/bin/zoho-cliq-fhs
 Icon=cliq
 Type=Application
 Categories=Network;InstantMessaging;
+StartupWMClass=cliq
 EOF
+
+    ln -s ${cliq-fhs}/bin/zoho-cliq-fhs $out/bin/zoho-cliq
   '';
 
   meta = {
-    description = "Zoho Cliq Desktop Client";
+    description = "Zoho Cliq Desktop Client (FHS, Xwayland)";
     homepage = "https://www.zoho.com/cliq/";
     license = lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    maintainers = [ ];
   };
 }
